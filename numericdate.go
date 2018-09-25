7114d5eb80c09d5e3bcce8031388ee345e048e06
Tiago van den Berg <tiagovdberg@gmail.com>
2018-09-25 00:13:53 -0300

    NumericalDate to Time

--- numericdate.go
+++ numericdate.go
@@ -7,3 +7,8 @@ import (
 func NumericDate(t time.Time) int64 {
 	return int64(t.Sub(time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC)).Seconds())
 }
+
+func Time(numericDate int64) time.Time {
+	duration := time.Duration(numericDate) * time.Second
+	return time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC).Add(duration)
+}
